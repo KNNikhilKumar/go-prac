04ea89be064d0a4410265dac0db5316f5ec6d414
nikhil <nikhilnaveenkn@gmail.com>
2024-08-06 21:42:49 +0530

    func returning func

--- firstclass/firstclass.go
+++ firstclass/firstclass.go
@@ -16,7 +16,19 @@ func perform(x int, y int, action func(int, int) int) int {
 	return action(x, action(x, y))
 }
 
+//func returning func Curried Function
+
+func selfConstruct(action func(int, int) int) func(int) int {
+	return func(x int) int {
+		return action(x, x)
+	}
+}
+
 func ExecFC() {
 	fmt.Println(perform(1, 2, add))
 	fmt.Println(perform(1, 2, mul))
+
+	//we use mul to calculate square
+	squareFunc := selfConstruct(mul)
+	fmt.Println(squareFunc(3))
 }
