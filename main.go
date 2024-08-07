9dc137e708b4a52abaed6382cd99d476af4cbff8
nikhil <nikhilnaveenkn@gmail.com>
2024-08-08 00:22:25 +0530

    tried godotenv

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "go-prac/firstclass"
+import (
+	"fmt"
+	"go-prac/firstclass"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	//1.FirstClass
 	firstclass.ExecFC()
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+	fmt.Println(port)
+
 }
